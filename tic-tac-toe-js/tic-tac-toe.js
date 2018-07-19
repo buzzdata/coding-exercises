2bb1039138466cb9c6bf74945eb542f7da45f69f
Patrick Rosales <patrick.rosales@gmail.com>
2018-07-19 19:30:12 -0400

    Added semicolons where missing.

--- tic-tac-toe-js/tic-tac-toe.js
+++ tic-tac-toe-js/tic-tac-toe.js
@@ -14,70 +14,70 @@ TicTacToe.prototype.winner = function() {
   }
 
   if (row2[0] == "o" && row2[1] == "o" && row2[2] == "o") {
-    return "o"
+    return "o";
   }
 
   if (row3[0] == "o" && row3[1] == "o" && row3[2] == "o") {
-    return "o"
+    return "o";
   }
 
   if (row1[0] == "x" && row1[1] == "x" && row1[2] == "x") {
-    return "x"
+    return "x";
   }
 
   if (row2[0] == "x" && row2[1] == "x" && row2[2] == "x") {
-    return "x"
+    return "x";
   }
 
   if (row3[0] == "x" && row3[1] == "x" && row3[2] == "x") {
-    return "x"
+    return "x";
   }
 
   // column checks
 
   if (row1[0] == "o" && row2[0] == "o" && row3[0] == "o") {
-    return "o"
+    return "o";
   }
 
   if (row1[1] == "o" && row2[1] == "o" && row3[1] == "o") {
-    return "o"
+    return "o";
   }
 
   if (row1[2] == "o" && row2[2] == "o" && row3[2] == "o") {
-    return "o"
+    return "o";
   }
 
   if (row1[0] == "x" && row2[0] == "x" && row3[0] == "x") {
-    return "x"
+    return "x";
   }
 
   if (row1[1] == "x" && row2[1] == "x" && row3[1] == "x") {
-    return "x"
+    return "x";
   }
 
   if (row1[2] == "x" && row2[2] == "x" && row3[2] == "x") {
-    return "x"
+    return "x";
   }
 
   // diagonal checks
 
   if (row1[0] == "o" && row2[1] == "o" && row3[2] == "o") {
-    return "o"
+    return "o";
   }
 
   if (row1[2] == "o" && row2[1] == "o" && row3[0] == "o") {
-    return "o"
+    return "o";
   }
 
   if (row1[0] == "x" && row2[1] == "x" && row3[2] == "x") {
-    return "x"
+    return "x";
   }
 
   if (row1[2] == "x" && row2[1] == "x" && row3[0] == "x") {
-    return "x"
+    return "x";
   }
 
-  return "draw"
+  return "draw";
 };
 
 module.exports = TicTacToe;
