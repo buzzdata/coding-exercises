41000f610589941a36ac400e1989ca1fe7de2691
Patrick Rosales <patrick.rosales@gmail.com>
2018-07-19 23:17:14 -0400

    Refactored victory checking. Added strict mode directive.

--- tic-tac-toe-js/tic-tac-toe.js
+++ tic-tac-toe-js/tic-tac-toe.js
@@ -1,4 +1,6 @@
 var TicTacToe = function(board) {
+  "use strict";
+
   this.board = board;
 };
 
@@ -14,11 +16,51 @@ TicTacToe.prototype.winner = function() {
   var rows = this.getRows(numberOfRows);
   var columns = this.getColumns(rows);
 
+  return this.checkWinner(rows, columns);
+};
+
+// Helper function to store the rows on the board.
+TicTacToe.prototype.getRows = function(size) {
+  var rows = [];
+
+  // Populate the row arrays.
+  for (var i = 0; i < size; i++) {
+    var row = [];
+
+    for (var j = 0; j < size; j++) {
+      row.push(this.board[i][j]);
+    }
+
+    rows.push(row);
+  }
+
+  return rows;
+};
+
+// Helper function to store the columns on the board.
+TicTacToe.prototype.getColumns = (rows) => {
+  var columns = [];
+
+  // Initialize the columns arrays.
+  rows.forEach(() => {
+    columns.push([]);
+  });
+
+  // Populate the column arrays.
+  for (var i = 0; i < rows.length; i++) {
+    for (var j = 0; j < rows.length; j++) {
+      columns[j][i] = rows[i][j];
+    }
+  }
+
+  return columns;
+};
+
+// Determines the outcome.
+TicTacToe.prototype.checkWinner = function(rows, columns) {
   var row1 = this.board[0];
   var row2 = this.board[1];
   var row3 = this.board[2];
-
-  // row checks
 
   if (row1[0] == "o" && row1[1] == "o" && row1[2] == "o") {
     return "o";
@@ -89,43 +131,6 @@ TicTacToe.prototype.winner = function() {
   }
 
   return "draw";
-};
-
-// Helper function to store the rows on the board.
-TicTacToe.prototype.getRows = function(size) {
-  var rows = [];
-
-  // Populate the row arrays.
-  for (var i = 0; i < size; i++) {
-    var row = [];
-
-    for (var j = 0; j < size; j++) {
-      row.push(this.board[i][j]);
-    }
-
-    rows.push(row);
-  }
-
-  return rows;
-};
-
-// Helper function to store the columns on the board.
-TicTacToe.prototype.getColumns = function(rows) {
-  var columns = [];
-
-  // Initialize the columns arrays.
-  rows.forEach(function() {
-    columns.push([]);
-  });
-
-  // Populate the column arrays.
-  for (var i = 0; i < rows.length; i++) {
-    for (var j = 0; j < rows.length; j++) {
-      columns[j][i] = rows[i][j];
-    }
-  }
-
-  return columns;
 };
 
 // Helper function for printing the board.
