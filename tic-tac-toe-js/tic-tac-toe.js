f32ac562098f2e4a668b1a8118af0b660e457fa4
Viral Patel <pviral810@gmail.com>
2018-08-01 00:29:39 -0400

    ES6 cleanup

--- tic-tac-toe-js/tic-tac-toe.js
+++ tic-tac-toe-js/tic-tac-toe.js
@@ -2,62 +2,62 @@ var TicTacToe = function(board) {
   this.board = board;
 };
 
-var hasWinningFormula = function(boxes) {
-  if ( boxes.every(v => v === boxes[0]) ) {
-    if ( boxes[0].trim() !== "" ) return true;
+var hasWinningFormula = function(rows) {
+  if (rows.every(v => v === rows[0])) {
+    if (rows[0].trim() !== "") return true;
   }
   return false;
-}
+};
 
 TicTacToe.prototype.winner = function() {
   var result;
 
   // row checks
-  this.board.forEach((boxes) => {
-    if ( hasWinningFormula(boxes) ) result = boxes[0];
+  this.board.forEach(rows => {
+    if (hasWinningFormula(rows)) result = rows[0];
   });
 
   if (result) return result;
 
   // backward diagonal checks
   var diagonals = [];
-  this.board.forEach( (boxes, i) => {
-    diagonals.push(boxes[i]);
+  this.board.forEach((rows, i) => {
+    diagonals.push(rows[i]);
   });
 
-  if ( hasWinningFormula(diagonals) ) result = diagonals[0];
+  if (hasWinningFormula(diagonals)) result = diagonals[0];
 
   if (result) return result;
 
   // column checks
-  for( var i = 0; i < this.board.length;i++) {
+  for (var i = 0; i < this.board.length; i++) {
     var columns = [];
-    this.board.forEach( (boxes) => {
-      columns.push(boxes[i]);
+    this.board.forEach(rows => {
+      columns.push(rows[i]);
     });
-    if ( hasWinningFormula(columns) ) result = columns[0];
+    if (hasWinningFormula(columns)) result = columns[0];
   }
 
   if (result) return result;
 
   // forward diagonal checks
   var diagonals = [];
-  this.board.forEach( (boxes, i) => {
+  this.board.forEach((rows, i) => {
     var TotalBoards = this.board.length - 1;
-    boxes.forEach((box, j) => {
-      if( j + i === TotalBoards ) {
+    rows.forEach((box, j) => {
+      if (j + i === TotalBoards) {
         diagonals.push(box);
       }
-    })
+    });
   });
 
-  if ( hasWinningFormula(diagonals) ) result = diagonals[0];
+  if (hasWinningFormula(diagonals)) result = diagonals[0];
 
-  if(result) return result;
+  if (result) return result;
 
-  // draw check
-  this.board.forEach(boxes => {
-    if ( boxes.some(v => v.trim() === "") ) result = "unfinished"
+  // unfinished check
+  this.board.forEach(rows => {
+    if (rows.some(v => v.trim() === "")) result = "unfinished";
   });
 
   if (result) return result;
